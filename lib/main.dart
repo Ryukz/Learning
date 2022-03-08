7bf6926a78937e3cf14752218390e0f2695c4064
Ankit Kumar <ankit.kumar2@bfhl.in>
2022-03-08 21:26:03 +0530

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const MyHomePage(title: 'Good morning Ryukz!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+int clicker = 0;
+void main() => runApp(MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Hello World'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Button Pressed!:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: Text("Hello $clicker")),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          clicker++;
+        },
+        backgroundColor: Colors.deepPurple,
+        child: Icon(Icons.alarm),
       ),
-    );
-  }
-}
-
-//Checking something!!!
+    )));
