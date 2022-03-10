9cb7580b1a138dae45827475764f98f6d87ffc62
Ankit Kumar <ankit.kumar2@bfhl.in>
2022-03-10 14:06:24 +0530

    added stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 int clicker = 0;
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'HOME',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              fontFamily: 'MyFont', fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        backgroundColor: Colors.deepPurple[600],
       ),
-      body: Center(child: Text("Hello $clicker")),
+      body: Center(
+          child: Text(
+        "This is Sample Text",
+        style: TextStyle(
+            color: Colors.deepPurple[600],
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+            fontFamily: 'MyFont',
+            fontSize: 22),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           clicker++;
@@ -18,4 +37,6 @@ void main() => runApp(MaterialApp(
         backgroundColor: Colors.deepPurple,
         child: Icon(Icons.alarm),
       ),
-    )));
+    ));
+  }
+}
