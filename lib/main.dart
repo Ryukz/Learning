0f9ee18d28e8a7d753d494f8beef6b938141e52f
Ankit Kumar <ankit.kumar2@bfhl.in>
2022-03-12 20:36:38 +0530

    my profile Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 int clicker = 0;
@@ -10,52 +12,149 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'HOME',
-          textAlign: TextAlign.left,
-          style: TextStyle(
-              fontFamily: 'MyFont', fontWeight: FontWeight.bold, fontSize: 22),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Profile Details',
+              style: TextStyle(
+                  fontSize: 18.5, fontFamily: 'MyFont2', letterSpacing: 1.5),
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.red, Colors.deepPurple],
+                    stops: [1.0, 0.2])),
+          ),
         ),
-        backgroundColor: Colors.deepPurple[600],
-      ),
-      // ignore: avoid_unnecessary_containers
-      body: Row(
-        children: [
-          Expanded(
-            child: Container(
-              child: Text('1'),
-              color: Colors.amberAccent,
-              padding: EdgeInsets.fromLTRB(30.0, 30.0, 60.0, 30.0),
-              margin: EdgeInsets.all(10.0),
+        body: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 45.0,
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.white12,
+                  radius: 70.0,
+                  child: Icon(
+                    Icons.person,
+                    size: 95,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Divider(
+                        height: 12.0,
+                        color: Colors.white,
+                        thickness: 0.8,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        height: 45,
+                        width: 400,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              ' Alex Morgan',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        height: 45,
+                        width: 400,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.email_outlined,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              ' abc345@gmail.com',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        height: 45,
+                        width: 400,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              ' 1234567890',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.red, Colors.deepPurple],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.2, 1.0]),
             ),
           ),
-          Expanded(
-            child: Container(
-              child: Text('2'),
-              color: Colors.pinkAccent,
-              padding: EdgeInsets.all(30.0),
-              margin: EdgeInsets.all(10.0),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              child: Text('3'),
-              color: Colors.orangeAccent,
-              padding: EdgeInsets.all(30.0),
-              margin: EdgeInsets.all(10.0),
-            ),
-          )
-        ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          clicker++;
-        },
-        backgroundColor: Colors.deepPurple,
-        child: Icon(Icons.alarm),
-      ),
-    ));
+    );
   }
 }
