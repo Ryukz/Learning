3f4228dd30791b387d3e685e1e414a4fe8b84e23
Ankit Kumar <ankit.kumar2@bfhl.in>
2022-03-15 06:19:44 +0530

    added worldtime api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, camel_case_types, must_be_immutable, prefer_const_constructors_in_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, camel_case_types, must_be_immutable, prefer_const_constructors_in_immutables, avoid_print, unused_import, unused_local_variable
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
@@ -11,20 +11,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getData() async {
-    //Example of asysnc code and http network request
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    print(response.body);
-    Map jsonResponse = jsonDecode(response.body);
-    print(jsonResponse['title']);
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+    Map data = jsonDecode(response.body);
+    String dateTime = data['datetime'];
+    print(dateTime);
+    String offset =
+        data['utc_offset'].toString().replaceAll("+", "").split(":")[0];
+    String offsetMin = '';
+    try {
+      offsetMin =
+          data['utc_offset'].toString().replaceAll("+", "").split(":")[1];
+    } catch (e) {
+      print(e);
+    }
+    print(offset);
+    DateTime now = DateTime.parse(dateTime);
+    now = now
+        .add(Duration(hours: int.parse(offset), minutes: int.parse(offsetMin)));
+    print(now);
   }
 
   @override
   void initState() {
     print("This is initFunction");
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
